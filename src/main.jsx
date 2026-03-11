6d88487375bc4ce6f79018d299e79ffb9aa5cac7
Prakhar sakhare~ <prakharsakhare2226@gmail.com>
2026-03-11 19:44:47 +0530

    try: fixing  deployement issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,11 @@
+// Shims for libraries that expect Node.js globals
+if (typeof global === 'undefined') {
+  window.global = window;
+}
+if (typeof process === 'undefined') {
+  window.process = { env: {} };
+}
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
